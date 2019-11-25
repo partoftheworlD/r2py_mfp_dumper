5f203828ac09c12d78854383f1f2e7edad39d68a
partoftheworlD <partoftheworldcs@gmail.com>
2019-11-25 23:18:05 +0300

    update

--- mfp_chapter_timer.py
+++ mfp_chapter_timer.py
@@ -4,13 +4,17 @@ import re
 re_temp = re.compile(r'0x[0-9a-f]{2,4}\]')
 
 def fnc_1():
-    r2 = r2pipe.open("UnityPlayer.dll", ["-B 0x0"])
-    sign_base = search(r2, "48893d........ff15........3935........")
-    sign_offset = search(r2, "0f11..........410f28..e8........410f28..f30f11....")
-    base = str(hex(get_ptr(r2, sign_base))).upper()[2:]
-    offset = str(hex(get_offset(r2, sign_offset))).upper()[2:]
-    print(f"[Chapter time address] - \"Unityplayer.dll\" : 0x{base}, 0x{offset}")
-    r2.quit()
+    try:
+        r2 = r2pipe.open("UnityPlayer.dll", ["-B 0x0"])
+        if r2:
+            sign_base = search(r2, "48893d........ff15........3935........")
+            sign_offset = search(r2, "0f11..........410f28..e8........410f28..f30f11....")
+            base = str(hex(get_ptr(r2, sign_base))).upper()[2:]
+            offset = str(hex(get_offset(r2, sign_offset))).upper()[2:]
+            print(f"[Chapter time address] - \"Unityplayer.dll\" : 0x{base}, 0x{offset}")
+        r2.quit()
+    except BrokenPipeError:
+        print("Put UnityPlayer.dll in the script folder.")
 
 def get_offset(h_r2, flag):
     h_r2.cmd('s ' + str(flag))
@@ -26,4 +30,4 @@ def search(h_r2, signature):
     return h_r2.cmdj('/xj ' + signature)[0]["offset"]
 
 if __name__ == '__main__':
-   fnc_1()
+    fnc_1()
